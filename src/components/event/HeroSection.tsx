df03677055c3fb121302a4e3edbf89530e58028b
Muhammad Subhan Khan <msk98.sk@gmail.com>
2025-07-10 05:12:19 +0500

    updated the hero section according to latest design

--- src/components/event/HeroSection.tsx
+++ src/components/event/HeroSection.tsx
@@ -9,6 +9,7 @@ import {
   faLocationDot,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
@@ -48,23 +49,39 @@ export default function HeroSection() {
 
   return (
     <div className="bg-[url(/hero_image_event.jpg)] bg-top bg-cover bg-no-repeat rounded-[32px] mx-4 lg:mx-8 pt-28 pb-12 px-[24px] lg:py-36 lg:px-36">
-      <h1 className="font-extrabold text-[42px] leading-[34.18px] md:text-[110px] md:leading-[105px] text-gray-50 w-full lg:w-[80%] xl:w-[70%]">
-        Euismod mauris diam mattis aenean.
-      </h1>
-      <div className="flex gap-2 lg:gap-6 mt-6">
-        <Link href="https://wwwthelimitlesswaynl.plugandpay.com/checkout/event">
-          <Button className="bg-[#E6C7FF] text-[#282828] py-6 px-3 md:px-6 flex-1 md:flex-none gap-[18px] cursor-pointer hover:scale-105">
-            Bestel je ticket <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-          </Button>
-        </Link>
-        <Link href="#agenda">
-          <Button
-            variant="outline"
-            className="bg-[#282828] text-gray-50 py-6 px-3 md:px-6 flex-1 md:flex-none gap-[18px] cursor-pointer hover:scale-105"
-          >
-            Meer informatie <FontAwesomeIcon icon={faArrowDown} />
-          </Button>
-        </Link>
+      <div className="grid grid-col-auto">
+        <div className="max-w-[950] col-span-2 xl:col-span-1">
+          <h1 className="font-extrabold text-[42px] leading-[34.18px] md:text-[110px] md:leading-[105px] text-gray-50">
+            Slimmer met geld. Vrijer in het leven.
+          </h1>
+          <p className="my-5 text-gray-50 text-lg md:text-[23px]">
+            Voor iedereen die meer wil halen uit crypto en andere assets, maar
+            vooral: uit zichzelf en het leven.
+          </p>
+        </div>
+        <div className="flex gap-2 lg:gap-6 row-start-2 col-start-1 self-start">
+          <Link href="https://wwwthelimitlesswaynl.plugandpay.com/checkout/event">
+            <Button className="bg-[#E6C7FF] text-[#282828] py-6 px-3 md:px-6 flex-1 md:flex-none gap-[18px] cursor-pointer hover:scale-105">
+              Bestel je ticket{" "}
+              <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+            </Button>
+          </Link>
+          <Link href="#agenda">
+            <Button
+              variant="outline"
+              className="bg-[#282828] text-gray-50 py-6 px-3 md:px-6 flex-1 md:flex-none gap-[18px] cursor-pointer hover:scale-105"
+            >
+              Meer informatie <FontAwesomeIcon icon={faArrowDown} />
+            </Button>
+          </Link>
+        </div>
+        <div className="flex gap-4 row-start-3 col-span-2 md:row-start-2 md:col-start-2 md:col-span-1 xl:col-start-2 xl:row-start-1 rounded-2xl p-4 self-start justify-self-end w-full md:max-w-[300px] mt-5 md:mt-0 bg-[#E6C7FF] justify-between">
+          <p className="text-lg">
+            Inclusief lancering van onze revolutionaire crypto app!
+          </p>
+
+          <Image src={"./icons/hero-logo.svg"} width={50} height={50} alt="" />
+        </div>
       </div>
 
       <div className="w-full xl:w-[80%] 2xl:w-[50%] mt-28">
@@ -91,17 +108,22 @@ export default function HeroSection() {
               <div className="text-[#525252]">14:00 - 18:00</div>
             </div>
           </div>
-          <div className="bg-gray-50 flex grow items-center rounded-[20px] p-[12px] lg:p-[16px] lg:pr-[32px] gap-4">
-            <FontAwesomeIcon
-              icon={faLocationDot}
-              width={24}
-              className="text-[#7F65CA]"
-            />
-            <div className="flex flex-col">
-              <div className="text-[#282828] font-semibold">Locatie</div>
-              <div className="text-[#525252]">Green Village - Nieuwegein</div>
+          <Link
+            href={"https://green-village.nl/ "}
+            className="bg-gray-50 grow rounded-[20px] p-[12px] lg:p-[16px] lg:pr-[32px]"
+          >
+            <div className="flex items-center gap-4">
+              <FontAwesomeIcon
+                icon={faLocationDot}
+                width={24}
+                className="text-[#7F65CA]"
+              />
+              <div className="flex flex-col">
+                <div className="text-[#282828] font-semibold">Locatie</div>
+                <div className="text-[#525252]">Green Village - Nieuwegein</div>
+              </div>
             </div>
-          </div>
+          </Link>
 
           <div className="bg-[#7F65CA] rounded-[20px] mt-2 p-[12px] lg:py-5 lg:px-4 flex w-full text-gray-50">
             <div className="flex flex-col flex-1 justify-center items-center border-r">
