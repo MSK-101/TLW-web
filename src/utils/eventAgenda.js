60590dd3dd9226f4c5d6f37a12913496cbbf389e
MSK-101 <msk98.sk@gmail.com>
2025-07-11 05:14:26 +0500

    update content

--- src/utils/eventAgenda.js
+++ src/utils/eventAgenda.js
@@ -1,43 +1,47 @@
 const eventAgenda = [
   {
-    "time": "14:00 - 14:30",
-    "title": "Inloop en ontvangst",
-    "description": "Koffie, badges, QR-goodie, muziek.",
+    time: "14:00 - 14:30",
+    title: "Inloop en ontvangst",
+    description: "Koffie/thee, badges, QR-goodie",
   },
   {
-    "time": "14:30 - 14:45",
-    "title": "Plenaire opening – Myrte & Tineke",
-    "description": "TLW-verhaal & routekaart voor de middag.",
+    time: "14:30 - 14:45",
+    title: "Plenaire opening – Myrte & Tineke",
+    description: "TLW-verhaal & routekaart voor de middag",
   },
   {
-    "time": "14:45 - 15:15",
-    "title": "Keynote – Tineke: Jouw route naar financiële vrijheid + macro economisch",
-    "description": "Inspiratie en strategie vanuit eigen reis + app als toolkit",
+    time: "14:45 - 15:15",
+    title: "Keynote – Tineke: Jouw route naar financiële vrijheid",
+    description: "Inspiratie en strategie vanuit eigen reis + app als toolkit",
   },
   {
-    "time": "15:15 - 16:00",
-    "title": "Workshopblok 1 (keuze uit 3 sessies - gericht op educatie, zelfinzicht en app functionaliteiten)",
-    "description": "Deelnemers kiezen vooraf 1 van de 3 sessies",
+    time: "15:15 - 16:00",
+    title:
+      "Workshopblok 1 (keuze uit 3 sessies - gericht op educatie, zelfinzicht en app functionaliteiten)",
+    description:
+      "Je kiest 1 van de 3 sessies, je ontvangt meer informatie hierover nadat je je ticket hebt gekocht.",
   },
   {
-    "time": "16:00 - 16:45",
-    "title": "Workshopblok 2 (keuze uit 3 sessies - gericht op concrete strategie en toepasbaarheid)",
-    "description": "Nieuwe ronde met andere 3 sessies",
+    time: "16:00 - 16:45",
+    title:
+      "Workshopblok 2 (keuze uit 3 sessies - gericht op concrete strategie en toepasbaarheid)",
+    description:
+      "Je kiest 1 van de 3 sessies, je ontvangt meer informatie hierover nadat je je ticket hebt gekocht.",
   },
   {
-    "time": "16:45 - 17:00",
-    "title": "Reset & zaalwissel naar plenaire ruimte",
-    "description": "Muziek, water, hergroeperen",
+    time: "16:45 - 17:00",
+    title: "Reset & zaalwissel naar plenaire ruimte",
+    description: "",
   },
   {
-    "time": "17:00 - 17:20",
-    "title": "App Reveal – Myrte & Tineke",
-    "description": "Live demo, countdown, user incentives + mini-actie",
+    time: "17:00 - 17:20",
+    title: "App Reveal – Myrte & Tineke",
+    description: "Live demo, countdown, user incentives + mini-actie",
   },
   {
-    "time": "17:20 - 18:00",
-    "title": "Borrel & activatieplein",
-    "description": "45 min drinks, muziek (achtergrond), goodie activatie, netwerken",
+    time: "17:20 - 18:00",
+    title: "Borrel & activatieplein",
+    description: "Hapje, drankje en netwerken + QR goodie activatie",
   },
 ];
 
