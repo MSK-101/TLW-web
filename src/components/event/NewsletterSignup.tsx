40ca437ce4ab961c10cf5e4302a4b5d3abaf3adc
MSK-101 <msk98.sk@gmail.com>
2025-07-15 06:08:56 +0500

    embed newsletter signup form

--- src/components/event/NewsletterSignup.tsx
+++ src/components/event/NewsletterSignup.tsx
@@ -1,60 +1,20 @@
 "use client";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import Script from "next/script";
 
 export default function NewsletterSignup() {
-  const [isChecked, setIsChecked] = useState(false);
-
-  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIsChecked(e.target.checked);
-  };
-
-  const handleIconClick = () => {
-    if (isChecked) {
-      // Handle email submission logic here
-      console.log("Email submitted");
-    }
-  };
-
   return (
-    <section className="flex justify-center text-[#282828] rounded-2xl lg:rounded-[32px] mx-4 lg:mx-0 mt-16 lg:mt-20 py-20 lg:py-40">
-      <div className="w-full px-6 lg:px-0 lg:w-2/4 text-center">
-        <h1 className="text-4xl lg:text-7xl mb-6 lg:mb-8 px-5 text-center">
+    <section className="flex justify-center text-[#282828] rounded-2xl lg:rounded-[32px] mx-4 lg:mx-0 mt-16 lg:mt-20 py-20 lg:pt-40">
+      <div className="w-full flex flex-col lg:flex-row">
+        <h1 className="text-4xl lg:text-6xl text-left sm:text-center lg:text-left 2xl:text-7xl px-5 w-full lg:w-1/2">
           Meld je aan en blijf op de hoogte van de ontwikkelingen.
         </h1>
-        <div className="flex items-center relative mb-4">
-          <input
-            type="email"
-            placeholder="janjansen@mail.com"
-            className="w-full bg-[#F0F0F0] rounded-full py-3 lg:py-4 pl-6 pr-14 focus:outline-[#7F65CA]"
+        <div className="_form_1 w-full lg:w-1/2 lg:-mt-8">
+          <Script
+            src="https://thelimitlesswaybv.activehosted.com/f/embed.php?id=1"
+            strategy="afterInteractive"
+            onError={(e) => console.error("Script failed to load", e)}
           />
-          <FontAwesomeIcon
-            icon={faPaperPlane}
-            width={20}
-            className={`cursor-pointer absolute right-4 ${
-              isChecked
-                ? "text-[#7F65CA] hover:text-[#5a4a9a]"
-                : "text-gray-400 cursor-not-allowed"
-            }`}
-            onClick={handleIconClick}
-          />
-        </div>
-        <div className="flex items-center justify-center">
-          <input
-            type="checkbox"
-            className="mr-2"
-            id="tnc_checkbox"
-            checked={isChecked}
-            onChange={handleCheckboxChange}
-          />
-          <label
-            className="text-gray-600 text-sm lg:text-base"
-            htmlFor="tnc_checkbox"
-          >
-            Ik ga akkoord met de algemene voorwaarden
-          </label>
         </div>
       </div>
     </section>
