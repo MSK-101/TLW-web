d0171c5c876ffe9e23c1041685ae506f5bb32d7b
MSK-101 <msk98.sk@gmail.com>
2025-07-22 00:29:08 +0500

    optimize footer section for pre launch

--- src/app/pre-launch/components/download-app-section.tsx
+++ src/app/pre-launch/components/download-app-section.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function DownloadAppSection() {
   return (
-    <div className="bg-[url(/background.png)] bg-bottom bg-cover bg-no-repeat rounded-2xl lg:rounded-b-4xl p-6 lg:px-[64px] lg:-mx-[76px]">
-      <div className="grid grid-cols-2">
-        <div className="col-auto self-center lg:pl-6 max-w-[750px]">
-          <h1 className="text-3xl md:text-6xl xl:text-7xl font-bold opacity-80 leading-[90%]">
+    <div className="bg-[url(/background.png)] bg-bottom bg-cover bg-no-repeat rounded-2xl lg:rounded-4xl p-6 lg:py-8 lg:px-[64px]">
+      <div className="grid grid-cols-2 space-x-2">
+        <div className="col-auto self-center lg:pl-8 max-w-[700px]">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl font-bold opacity-80 -mr-8 sm:-mr-0 leading-[100%] md:leading-[90%]">
             Download de app en werk aan je eigen toekomst.
           </h1>
-          <p className="mt-8">
+          <p className="mt-2 md:mt-8 text-gray-700 lg:mr-10">
             De app wordt gelanceerd op donderdag 25 september 2025! Non ultrices
             ac quis id tincidunt cursus.
           </p>
@@ -19,7 +19,7 @@ export default function DownloadAppSection() {
             width={1106}
             height={822}
             alt=""
-            className="h-full w-full translate-y-6 translate-x-6 lg:translate-x-0 max-h-[420px] object-contain scale-105 lg:scale-120 animate-slide-in-right"
+            className="h-full w-full translate-y-2 lg:translate-y-18 translate-x-4 max-h-[500px] object-contain scale-110 lg:scale-125 animate-slide-in-right"
           />
         </div>
       </div>
