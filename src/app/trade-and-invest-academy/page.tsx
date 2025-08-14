a090308c6215a5e009485969017140656afac395
Muhammad Subhan Khan <msk98.sk@gmail.com>
2025-08-14 14:25:04 +0500

    code refactor

--- src/app/trade-and-invest-academy/page.tsx
+++ src/app/trade-and-invest-academy/page.tsx
@@ -2,6 +2,8 @@
 
 import { useEffect, useState } from "react";
 import { getStrapiContentClient } from "@/lib/strapi-client";
+import PageLoader from "@/components/ui/page-loader";
+
 import tradeAndInvestAcademyCache from "@/utils/api-cached-responses/trade-and-invest-academy.json";
 
 import BenefitsSection from "../crypto-alerts/components/benefits-section";
@@ -146,20 +148,7 @@ export default function TradeAndInvestAcademyPage() {
   }, []);
 
   if (loading) {
-    return (
-      <div className="min-h-screen bg-[#282828] p-[24px] flex items-center justify-center fixed inset-0 overflow-hidden">
-        <div className="bg-white p-8 rounded-xl shadow-lg w-[400px]">
-          <h2 className="text-lg md:text-2xl font-semibold mb-4">
-            Page Loading...
-          </h2>
-          <div className="space-y-3">
-            <div className="h-6 bg-gray-300 rounded animate-pulse"></div>
-            <div className="h-6 bg-gray-300 rounded animate-pulse w-5/6"></div>
-            <div className="h-6 bg-gray-300 rounded animate-pulse"></div>
-          </div>
-        </div>
-      </div>
-    );
+    return <PageLoader loading={loading} />;
   }
 
   return (
