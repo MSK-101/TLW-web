5f6b09996e6584cace3c763c172b521d31872808
MSK-101 <msk98.sk@gmail.com>
2025-08-15 06:21:34 +0500

    disable eslint

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    // Disable ESLint during builds
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'tlw-cms-prd.s3.eu-west-1.amazonaws.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "tlw-cms-prd.s3.eu-west-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
