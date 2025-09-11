f5ae872d6439c15f801c2c2b9bcf4cecccb4dfce
MSK-101 <msk98.sk@gmail.com>
2025-09-11 18:38:29 +0500

    minor text tweaks

--- src/utils/eventAgenda.js
+++ src/utils/eventAgenda.js
@@ -1,45 +1,43 @@
 const eventAgenda = [
   {
-    time: "14:00 - 14:30",
+    time: "13:45 – 14:15",
     title: "Inloop en ontvangst",
     description: "Koffie/thee, badges, QR-goodie",
   },
   {
-    time: "14:30 - 14:45",
+    time: "14:15 – 14:30",
     title: "Plenaire opening – Myrte & Tineke",
     description: "TLW-verhaal & routekaart voor de middag",
   },
   {
-    time: "14:45 - 15:15",
+    time: "14:30 – 15:00",
     title: "Keynote – Tineke: Jouw route naar financiële vrijheid",
     description: "Inspiratie en strategie vanuit eigen reis + app als toolkit",
   },
   {
-    time: "15:15 - 16:00",
-    title:
-      "Workshopblok 1 (keuze uit de 6 aangeboden sessies)",
+    time: "15:00 – 15:45",
+    title: "Workshopblok 1 (keuze uit 6 sessies, max. 50 p.p.)",
     description:
       "Je geeft je voorkeur op nadat je je ticket hebt gekocht - er is per workshop ruimte voor maximaal 50 mensen - en je ontvangt in aanloop naar het event meer informatie. Alle workshops worden opgenomen, zodat je alles sowieso terug kunt kijken!",
   },
   {
-    time: "16:00 - 16:45",
-    title:
-      "Workshopblok 2 (keuze uit de 6 aangeboden sessies)",
-    description:
-      "Je geeft je voorkeur op nadat je je ticket hebt gekocht - er is per workshop ruimte voor maximaal 50 mensen - en je ontvangt in aanloop naar het event meer informatie. Alle workshops worden opgenomen, zodat je alles sowieso terug kunt kijken!",
-  },
-  {
-    time: "16:45 - 17:00",
-    title: "Reset & zaalwissel naar plenaire ruimte",
+    time: "15:45 – 16:00",
+    title: "Zaalwissel om naar je tweede workshop te gaan",
     description: "",
   },
   {
-    time: "17:00 - 17:20",
+    time: "16:00 – 16:45",
+    title: "Workshopblok 2 (keuze uit 6 sessies, max. 50 p.p.)",
+    description:
+      "Je geeft je voorkeur op nadat je je ticket hebt gekocht - er is per workshop ruimte voor maximaal 50 mensen - en je ontvangt in aanloop naar het event meer informatie. Alle workshops worden opgenomen, zodat je alles sowieso terug kunt kijken!",
+  },
+  {
+    time: "16:45 – 17:05",
     title: "App Reveal – Myrte & Tineke",
     description: "Live demo van de app en lanceringsactie",
   },
   {
-    time: "17:20 - 18:00",
+    time: "17:05 – 18:00",
     title: "Borrel & downloaden + activeren app",
     description: "Hapje, drankje, netwerken en de app uitproberen.",
   },
