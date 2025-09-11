d313efde6e30bf31271bc8cebe02009515b3e1b7
MSK-101 <msk98.sk@gmail.com>
2025-09-11 21:02:28 +0500

    Update GTM

--- src/components/GTM.tsx
+++ src/components/GTM.tsx
@@ -9,11 +9,11 @@ export default function GTM() {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-M425RH79');`}
+})(window,document,'script','dataLayer','GTM-WRLRHNHT');`}
       </Script>
       <noscript>
         <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-M425RH79"
+          src="https://www.googletagmanager.com/ns.html?id=GTM-WRLRHNHT"
           height="0"
           width="0"
           style={{ display: "none", visibility: "hidden" }}
